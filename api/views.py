c3e5a6d07425617282b77886dd9ead153cdef9e1
Kevin Oommen <kevinoommenanish@gmail.com>
2023-04-26 15:39:42 +0530

    Fixed issue: Changed static path of data loader to fix chatbot response

--- api/views.py
+++ api/views.py
@@ -12,7 +12,9 @@ def chatbot_response(request):
     from llama_index import SimpleDirectoryReader
     from llama_index import GPTSimpleVectorIndex
     os.environ['OPENAI_API_KEY'] = "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"
-    documents = SimpleDirectoryReader(r'C:\Users\Kevin Oommen Anish\OneDrive\Desktop\climbout_backend\api\chatdata').load_data()
+    cwd=os.getcwd()
+    cwd+='\\api\\chatdata'
+    documents = SimpleDirectoryReader(cwd).load_data()
     index = GPTSimpleVectorIndex.from_documents(documents)
     resp = index.query(request.body.decode('utf-8'))
 
